c930b3fa5aa33655a792aca8c7722219ef4937f9
= <=>
2022-11-20 16:15:04 +0000

    first test of 3 col bootstrap

--- camwtf/settings.py
+++ camwtf/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #
-    'wtf.apps.WtfConfig'
+    'wtf.apps.WtfConfig',
+    'home.apps.HomeConfig'
 ]
 
 MIDDLEWARE = [
