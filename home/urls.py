c806bb7ed0cd71b1de4df7f4918c4bf2baf084f4
lea nicholson <lea@jayls.com>
2022-11-21 11:34:38 +0000

    first tag in a template working

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
-
+from .import views
 app_name='home'
 
 urlpatterns = [
     # pre-defined class from Django
     path('', TemplateView.as_view(template_name='home/main.html')),
+    path('tags', views.tags, name = 'tags'),
+    path('tags/<int:tagNum>', views.tags, name = 'tags'),
+
 ]
