e0e44fde6ebfb604e06f323dd7f545865b8509fa
lea nicholson <lea@jayls.com>
2022-11-28 13:18:29 +0000

    Moving to mac desktop  Changes to be committed: 	modified:   home/templates/home/main.html 	modified:   wtf/urls.py

--- wtf/urls.py
+++ wtf/urls.py
@@ -5,8 +5,8 @@ app_name='home'
 
 urlpatterns = [
     # pre-defined class from Django
-    path('', TemplateView.as_view(template_name='home/main.html')),
-    path('tags', views.tags, name = 'tags'),
+    #path('', TemplateView.as_view(template_name='home/main.html')),
+    path('', views.tags, name = 'tags'),
     path('tags/<int:tagNum>', views.tags, name = 'tags'),
 
 ]
