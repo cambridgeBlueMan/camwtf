e63b185b3cf7649740a7b1a62dc23ad5fc9a76d7
= <=>
2022-12-06 13:35:25 +0000

    preliminary select by intergroup select by town added Changes to be committed: 	modified:   wtf/urls.py 	modified:   wtf/views.py

--- wtf/urls.py
+++ wtf/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # pre-defined class from Django
     #path('', TemplateView.as_view(template_name='home/main.html')),
     path('', views.main, name = 'main'),
-    #path('tags/<int:tagNum>', views.tags, name = 'tags'),
-
+    path('intergroup/<int:ig>', views.intergroup, name = 'intergroup'),
+    path('town/<int:town>', views.town, name = 'town_id'),
+    path('town/<str:town>', views.town, name = 'town'),
 ]
--- wtf/views.py
+++ wtf/views.py
@@ -10,12 +10,37 @@ from .models import Region, Ig, Group, Venue, Meeting
 
 def main(request):
     meetings = Meeting.objects.all()
-    print(Meeting.objects.all)
     template = loader.get_template('home/main.html')
     context = {
-        'aTag': meetings,
         'meetings': meetings
+    }
+    return HttpResponse(template.render(context, request))
 
+def intergroup(request, ig):
+    """
+    Takes an integer representing an intergroup and filters meetings based on
+    that intergroup
+    """
+    meetings = Meeting.objects.filter(group__ig = ig)
+    template = loader.get_template('home/main.html')
+    context = {
+        'meetings': meetings
+    }
+    return HttpResponse(template.render(context, request))
+
+def town(request, town):
+    """ takes either an integer representing a town or string of the town name 
+    and returns all meetings in that town """
+    
+    if type(town) is str:
+        meetings = Meeting.objects.filter(venue__town__name = town)
+    
+    if type(town) is int:
+        meetings = Meeting.objects.filter(venue__town__id = town)
+    
+    template = loader.get_template('home/main.html')
+    context = {
+        'meetings': meetings
     }
     return HttpResponse(template.render(context, request))
 
