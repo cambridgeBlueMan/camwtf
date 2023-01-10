9878d8960a2b27ae8c3e7adad073534a24917025
= <=>
2023-01-10 13:11:28 +0000

    move to mysql stage 1

--- camwtf/settings.py
+++ camwtf/settings.py
@@ -83,8 +83,13 @@ WSGI_APPLICATION = 'camwtf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'wtf',
+        'USER': 'root',
+        'PASSWORD': 'figroll48',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
